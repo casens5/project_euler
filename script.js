03de1593e906c39914f7b2406cb87729315ed404
rudy ordoyne <rudolfordoyne@protonmail.com>
2019-09-06 17:46:04 -0400

    fixes

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 "use strict";
 
+const dom = {
+  problemSelector: document.querySelector("#problemSelector"),
+  statementDiv: document.querySelector("#statementDiv"),
+  codeDiv: document.querySelector("#codeDiv"),
+  dataDiv: document.querySelector("#dataDiv"),
+  outputDiv: document.querySelector("#outputDiv"),
+  executeBtn: document.querySelector("#executeBtn"),
+  clearBtn: document.querySelector("#clearBtn")
+};
+
 const problemsObj = {
   "-": {
     codeSrc: null,
     statement: null,
-    code: null
+    code: null,
+    data: null
   }
 };
 
-clearBtn.onclick = function() {
+dom.clearBtn.onclick = function() {
   displayAnswer(null);
 };
 
-problemSelector.addEventListener("change", function(event) {
+dom.problemSelector.addEventListener("change", function(event) {
   var problem = event.target.value; // this is a string
   console.log("switch to problem ", problem);
   if (problemsObj[problem] === undefined) {
@@ -28,18 +39,26 @@ function buildExpandos() {
   targets.forEach(function(item) {
     let label = document.createElement("span");
     let content = document.createElement("div");
+    let openBox = document.createElement("span");
+    let text = document.createElement("span");
+    item.show = true;
     content.classList = "content";
     label.classList = "label";
-    label.textContent = labels.shift();
+    openBox.classList = "open-box";
+    text.textContent = labels.shift();
+    openBox.textContent = "[-]";
     item.appendChild(label);
     item.appendChild(content);
+    label.appendChild(openBox);
+    label.appendChild(text);
     label.addEventListener("click", function(event) {
-      item.children[1].classList.toggle("hidden");
+      toggleField(item, "toggle");
     });
   });
-  codeDiv.children[1].classList.add("code", "harold", "hidden");
+  dom.codeDiv.children[1].classList.add("code");
 }
 
+// ten indents is too many god damn indents
 function fetchProblem(id) {
   problemsObj[id] = {};
   fetch(`./problems/statements/${id}.html`).then(function(response) {
@@ -62,11 +81,43 @@ function fetchProblem(id) {
               console.log("script assigned");
             })
             .then(function() {
-              loadProblemElements(id);
+              fetch(`./problems/data/${id}.txt`)
+                .then(function(response) {
+                  if (response.statusText == "File not found") {
+                    problemsObj[id].data = null;
+                  } else {
+                    let text = response.text().then(function(text) {
+                      problemsObj[id].data = text;
+                      console.log("problem data evaluated");
+                    });
+                  }
+                })
+                .then(function() {
+                  loadProblemElements(id);
+                });
             });
         });
       });
   });
+}
+
+function toggleField(node, onOrOff) {
+  if (onOrOff == "toggle") {
+    if (node.show) {
+      onOrOff = "off";
+    } else {
+      onOrOff = "on";
+    }
+  }
+  if (onOrOff == "on") {
+    node.show = true;
+    node.children[0].children[0].textContent = "[-]";
+    node.children[1].classList.remove("hidden");
+  } else {
+    node.show = false;
+    node.children[0].children[0].textContent = "[+]";
+    node.children[1].classList.add("hidden");
+  }
 }
 
 function loadProblemElements(id) {
@@ -74,20 +125,23 @@ function loadProblemElements(id) {
   script.src = problemsObj[id].codeSrc;
   script.async = false;
   script.onload = function() {
-    executeBtn.onClick = function() {
+    dom.executeBtn.onclick = function() {
+      console.log("i am being clicked");
       displayAnswer(window[`euler${id}`]());
     };
   };
   document.head.appendChild(script);
-  statementDiv.children[1].innerHTML = problemsObj[id].statement;
-  codeDiv.children[1].innerHTML = problemsObj[id].code;
-  statementDiv.children[1].classList.remove("hidden");
-  codeDiv.children[1].classList.remove("hidden");
+  dom.statementDiv.children[1].innerHTML = problemsObj[id].statement;
+  dom.codeDiv.children[1].innerHTML = problemsObj[id].code;
+  toggleField(dom.statementDiv, "on");
+  toggleField(dom.codeDiv, "on");
+  toggleField(dom.dataDiv, "off");
+  displayAnswer(null);
   console.log("DOM filled with problem elements");
 }
 
 function displayAnswer(string) {
-  outputDiv.textContent = string;
+  dom.outputDiv.textContent = string;
 }
 
 buildExpandos();
