d7b05a7c6f7407534979166c0a17943333b1ca38
rudy ordoyne <rudolfordoyne@protonmail.com>
2019-09-13 12:37:32 -0400

    fix euler008()

--- problems/solutions/008.js
+++ problems/solutions/008.js
@@ -1,7 +1,5 @@
 "use strict";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 function euler008(bigNum = problemsObj["008"].data) {
   let unbrokenString = bigNum.split("\n").join("");
   let removedZeroes = unbrokenString.split(/0+/);
@@ -24,9 +22,3 @@ function euler008(bigNum = problemsObj["008"].data) {
   });
   return greatestProduct;
 }
-=======
-function euler008(bigNum = problemsObj["008"]) {}
->>>>>>> parent of 547c38a... adjust minor CSS elements, semantic HTML tags
-=======
-function euler008(bigNum = problemsObj["008"]) {}
->>>>>>> parent of 547c38a... adjust minor CSS elements, semantic HTML tags
