301aedca2a01d331116aa820509d7b7594c5f65b
rudy ordoyne <rudolfordoyne@protonmail.com>
2019-09-13 11:53:26 -0400

    add euler008()

--- problems/solutions/008.js
+++ problems/solutions/008.js
@@ -1,3 +1,24 @@
 "use strict";
 
-function euler008(bigNum = problemsObj["008"].data) {}
+function euler008(bigNum = problemsObj["008"].data) {
+  let unbrokenString = bigNum.split("\n").join("");
+  let removedZeroes = unbrokenString.split(/0+/);
+  removedZeroes = removedZeroes.filter(string => string.length > 12);
+  let greatestProduct = 1;
+
+  const reducer = (acc, val) => {
+    return acc * Number(val);
+  };
+  removedZeroes.forEach(function(string) {
+    for (let i = 13; i <= string.length; i++) {
+      let product = string
+        .substring(i - 13, i)
+        .split("")
+        .reduce(reducer, 1);
+      if (greatestProduct < product) {
+        greatestProduct = product;
+      }
+    }
+  });
+  return greatestProduct;
+}
